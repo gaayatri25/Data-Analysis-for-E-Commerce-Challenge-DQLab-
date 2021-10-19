dfba5b7d2f3d86bf9d053235b755e4229012f01e
gayatri <49824841+gaayatri25@users.noreply.github.com>
2021-10-20 02:54:14 +0700

    Add files via upload

---
+++ number 9-13/number 12 (Transaksi Besar di Desember 2019).sql
@@ -0,0 +1,6 @@
+-- Transaksi Besar di Desember 2019
+SELECT nama_user AS nama_pembeli, total AS transaksi, created_at AS tanggal_transaksi
+FROM orders
+INNER JOIN users ON buyer_id = user_id
+WHERE created_at >= '2019-12-01' AND created_at < '2020-01-01' AND total >= 20000000
+ORDER BY 1
