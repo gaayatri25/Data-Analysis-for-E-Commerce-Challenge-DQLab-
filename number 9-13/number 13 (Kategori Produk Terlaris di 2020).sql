edb881ee0afb32fca4f6da774215be52b947e602
gayatri <49824841+gaayatri25@users.noreply.github.com>
2021-10-20 02:58:31 +0700

    Add files via upload

---
+++ number 9-13/number 13 (Kategori Produk Terlaris di 2020).sql
@@ -0,0 +1,8 @@
+SELECT category, SUM(quantity) AS quantity, SUM(price) AS price
+FROM orders
+INNER JOIN order_details USING(order_id)
+INNER JOIN products USING(product_id)
+WHERE created_at >= '2020-01-01' AND delivery_at IS NOT NULL
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5
