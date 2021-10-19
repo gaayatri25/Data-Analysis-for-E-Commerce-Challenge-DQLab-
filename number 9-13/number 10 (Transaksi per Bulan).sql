0400f6b1566c1187c02a1c3f40ddd9a2c0338985
gayatri <49824841+gaayatri25@users.noreply.github.com>
2021-10-20 03:02:54 +0700

    Delete number 10 (Transaksi per Bulan).sql

--- number 9-13/number 10 (Transaksi per Bulan).sql
+++
@@ -1,7 +0,0 @@
--- Transaksi per bulan di tahun 2020:
-SELECT EXTRACT(YEAR_MONTH FROM created_at) AS tahun_dan_bulan, COUNT(1) AS jumlah_transaksi, 
-SUM(total) AS total_nilai_transaksi
-FROM orders
-WHERE created_at >= '2020-01-01'
-GROUP BY 1
-ORDER BY 1
