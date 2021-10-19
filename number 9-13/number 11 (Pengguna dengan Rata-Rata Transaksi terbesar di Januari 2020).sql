92bba02854907bac21c1034e04d29eef2fb1ccbf
gayatri <49824841+gaayatri25@users.noreply.github.com>
2021-10-20 02:50:55 +0700

    Add files via upload

---
+++ number 9-13/number 11 (Pengguna dengan Rata-Rata Transaksi terbesar di Januari 2020).sql
@@ -0,0 +1,8 @@
+-- 10 pembeli dengan rata-rata nilai transaksi terbesar yang bertransaksi minimal 2 kali di Januari 2020:
+SELECT buyer_id, COUNT(1) AS jumlah_transaksi, AVG(total) AS rata2_nilai_transaksi
+FROM orders
+WHERE created_at >= '2020-01-01' AND created_at < '2020-02-01'
+GROUP BY 1
+HAVING COUNT(1) >=  2 
+ORDER BY 3 DESC
+LIMIT 10
